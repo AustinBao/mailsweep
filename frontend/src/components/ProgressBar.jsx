ec60a0464d936c58f5bfb32b4c1ebd974c27d4b2
Galileo Kim <galileokim@Galileos-MacBook-Pro.local>
2025-06-03 21:01:14 -0600

    if progress bar is finished message appears indicating inbox is read

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,7 +1,7 @@
 const ProgressBar = ({ progress }) => {
     return (
         <div className="d-flex flex-column align-items-center" style={{ width: "100%" }}>
-            <p className="mb-1 fw-bold">Inbox Progress</p>
+            <p className="mb-1 fw-bold">{ progress === 100 ? 'Inbox Read' : 'Inbox Progress'}</p>
             <div className="progress mb-3" style={{ height: "10px", width: "100%" }}>
                 <div 
                     className="progress-bar" 

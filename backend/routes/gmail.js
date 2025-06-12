0948e2c5a308fd9de09f23ad763b21d238adf845
Galileo Kim <galileokim@Galileos-MacBook-Pro.local>
2025-06-11 20:05:14 -0600

    progress bar basically done just need to make sure totalMailRead is synced properly

--- backend/routes/gmail.js
+++ backend/routes/gmail.js
@@ -10,6 +10,7 @@ router.get("/userinfo", async (req, res) => {
   if (!req.isAuthenticated()) {
     return res.status(401).send("Not Authenticated")
   }
+
   const accessToken = req.user.accessToken
   const userId = req.user.id
 
@@ -18,10 +19,18 @@ router.get("/userinfo", async (req, res) => {
   const gmail = google.gmail({ version: "v1", auth: oauth2Client})
 
   try {
-    const result = await gmail.users.getProfile({userId: 'me'})
+    const profile = await gmail.users.getProfile({userId: 'me'})
     // const messagesTotal = result.data.messagesTotal
-    const threadsTotal = result.data.threadsTotal
-    res.status(200).json(threadsTotal);
+    const threadsTotal = profile.data.threadsTotal
+
+    const result = await db.query(`SELECT total_mail_parsed FROM users WHERE id = $1`, [userId])
+
+    const totalMailParsed = result.rows[0]?.total_mail_parsed ?? 0
+
+    res.status(200).json({
+      threadsTotal,
+      totalMailParsed
+    });
 
   } catch (err) {
     console.log(err)
@@ -43,7 +52,7 @@ router.post('/delete', async (req, res) => {
   if (!req.isAuthenticated()) {
     return res.status(401).send("Not authenticated");
   }
-  const subscription_id = req.body.subscription_id;
+  const subscription_id = req.body.subscription_id; 
 
   // mark subscription as deleted in db to change card's bg color
   await db.query("UPDATE subscriptions SET is_deleted = true WHERE id = $1", [subscription_id]);
@@ -58,6 +67,7 @@ router.post('/delete', async (req, res) => {
   if (!emailIds.length) {
     return res.status(404).json({ message: 'No emails to delete.' });
   }
+  const deleteCount = emailIds.length
   // setup google api
   const oauth2Client = new google.auth.OAuth2( process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "http://localhost:3001/auth/google/callback" );
   oauth2Client.setCredentials({ access_token: req.user.accessToken, refresh_token: req.user.refreshToken });
@@ -68,6 +78,8 @@ router.post('/delete', async (req, res) => {
 
     // Delete removed emails from mail_to_delete table so email counter is accurate
     await db.query(`DELETE FROM mail_to_delete WHERE subscription_id = $1`, [subscription_id]);
+    await db.query(`UPDATE users SET total_mail_parsed = GREATEST(total_mail_parsed - $1, 0)
+      WHERE id = $2`, [deleteCount, req.user.id])
 
     res.status(200).json(deleteResult)
   } catch (err){
@@ -83,6 +95,7 @@ async function saveSubscriptionsToDB (userId, sender, sender_address, unsubLink,
     [userId, email_id, sender, sender_address, unsubLink, false, false, null, domain_pic, latest_date]
   );
 
+  //idk what this does
   if (result.rows.length === 0) {
     const subResult = await db.query(`
       SELECT id FROM subscriptions WHERE user_id = $1 AND sender_address = $2
@@ -98,6 +111,7 @@ async function saveSubscriptionsToDB (userId, sender, sender_address, unsubLink,
          [subscriptionId, email_id]);
     }
   }
+
 }
 
 router.get("/", async (req, res) => {
@@ -135,6 +149,11 @@ router.get("/", async (req, res) => {
     const nextToken  = result.data.nextPageToken
     if (nextToken ) {
       await savePageToken(userId, nextToken ); // save the new page token to page_token
+      await db.query(`
+        UPDATE users
+        SET total_mail_parsed = total_mail_parsed + 5
+        WHERE id = $1
+        `, [userId])
     }
 
     let unsubLinks = []
